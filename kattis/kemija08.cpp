3b90048aff52080ce4f4cdbef18e3f541da0a688
Bao Ho <pippi.blockhead@gmail.com>
2016-11-27 23:46:39 +0700

    Create kemija08.cpp

---
+++ kattis/kemija08.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool checkVowel(char c) {
+	return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	//freopen("inp.txt", "r", stdin);
+	
+	string s, res;
+	getline(cin, s);
+	for (int i = 0; i < s.size(); i++) {
+		res += s[i];
+		if (checkVowel(s[i])) {	
+			i += 2;
+		}
+	}
+	
+	cout << res;
+
+	return 0;
+}
