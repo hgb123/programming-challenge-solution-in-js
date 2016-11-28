44d51087d9758648139ed826e3674203e5e2eac6
Bao Ho <pippi.blockhead@gmail.com>
2016-11-28 16:12:35 +0700

    Create reversebinary.cpp

---
+++ kattis/reversebinary.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    //freopen("inp.txt","r",stdin);
+    unsigned long int n;
+    cin >> n;
+    
+    bitset<32> tempN(n);
+    string tempS(tempN.to_string());
+    while (tempS[0] == '0') {
+        tempS.erase(tempS.begin());
+    }
+    reverse(tempS.begin(),tempS.end());
+    bitset<32> res(tempS);
+    
+    cout << res.to_ulong() << endl; 
+    
+    return 0;
+}
