cdea1137b2c82261192a928e9fb93f98ae19d6a6
Bao Ho <pippi.blockhead@gmail.com>
2021-02-02 21:53:02 +0700

    add read input

---
+++ shared/read-input.js
@@ -0,0 +1,32 @@
+/*
+In local, run:
+  node index.js < input.txt
+*/
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString
+    .replace(/\s*$/, "")
+    .split("\n")
+    .map((str) => str.replace(/\s*$/, ""));
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function main() {
+  // process.stdout.write(inputString); // must be in string
+  // console.log(inputString);
+}
