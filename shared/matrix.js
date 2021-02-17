59ba2c1a470c9ee6887f188f1c49c41199e83864
Bao Ho <pippi.blockhead@gmail.com>
2021-02-17 23:38:07 +0700

    update matrix transpose

--- shared/matrix.js
+++ shared/matrix.js
@@ -1,13 +1,3 @@
-function transpose(arrOfArr) {
-  const N = arrOfArr.length;
-  const M = arrOfArr[0].length;
-  const res = [];
-  for (let i = 0; i < M; i++) {
-    let temp = [];
-    for (let j = 0; j < N; j++) {
-      temp.push(arrOfArr[j][i]);
-    }
-    res.push(temp);
-  }
-  return res;
+function transpose(matrix) {
+  return matrix[0].map((_, colIdx) => matrix.map((row) => row[colIdx]));
 }
