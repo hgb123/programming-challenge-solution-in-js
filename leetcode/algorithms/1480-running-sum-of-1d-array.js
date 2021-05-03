1c3b4d67746fe9832485daf4c731504126688360
Bao Ho <pippi.blockhead@gmail.com>
2021-05-03 22:24:04 +0700

    add leetcode may day 3

--- leetcode/algorithms/1480-running-sum-of-1d-array.js
+++ leetcode/algorithms/1480-running-sum-of-1d-array.js
@@ -4,6 +4,17 @@ LeetCode > Running Sum of 1d Array
 Tags: array
 */
 
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function (nums) {
+  for (let i = 1; i < nums.length; i++) {
+    nums[i] += nums[i - 1];
+  }
+  return nums;
+};
+
 var runningSum = function (nums) {
   const prefixSums = [];
   let sum = 0;
