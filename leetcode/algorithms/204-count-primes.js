f0ce9615085b5dc2114eb5f345f0a8d259c849f7
Bao Ho <pippi.blockhead@gmail.com>
2021-05-10 16:21:02 +0700

    add leetcode may day 10

---
+++ leetcode/algorithms/204-count-primes.js
@@ -0,0 +1,26 @@
+/*
+LeetCode > Count Primes
+
+Tags: math
+*/
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function (n) {
+  return sieveOfEratosthenes(n).filter(Boolean).length;
+};
+
+const sieveOfEratosthenes = (n) => {
+  const prime = new Uint8Array(n).fill(true);
+  prime[0] = prime[1] = false;
+  for (let i = 2; i * i <= n; i++) {
+    if (prime[i]) {
+      for (let k = i * i; k <= n; k += i) {
+        prime[k] = false;
+      }
+    }
+  }
+  return prime;
+};
